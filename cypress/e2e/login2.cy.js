984af0956d8492cc2a894a3377fe229d3c5385eb
Cid Huang <cid@msbt.com.tw>
2023-04-30 18:37:16 +0800

    add: check top-button-list labels

--- cypress/e2e/login2.cy.js
+++ cypress/e2e/login2.cy.js
@@ -11,13 +11,25 @@ context('Login', () => {
     })
   })
   //*
-  it('group', () => {
+  it('group item count', () => {
     cy.visit('https://portal.vortexcloud.com/#/group')
     cy.get('.top-button-list').children().should('have.length', 6)
+    let topButtonLabel = [
+      ' View ',
+      ' Devices ',
+      ' Deep search ',
+      ' Message center ',
+      ' Archive '
+    ]
+  cy.get('.top-button-list').children().find('.label').find('h5').then(item => {
+      for(let i=0;i<item.length;i++) {
+        cy.log(i, item[i].textContent)
+        expect(item[i].textContent).to.equal(topButtonLabel[i])
+      }
+    })
   })
-  //*/
   //*
-  it('device', () => {
+  it('device count', () => {
     cy.visit('https://portal.vortexcloud.com/#/device/management')
 
     cy.get('.device-group-list').children().then(item => {
@@ -26,19 +38,13 @@ context('Login', () => {
         return parseInt(str.substring(str.lastIndexOf('(',)+1, str.lastIndexOf(')')))
       }
 
-      cy.log('list', item.length)
       let total = getNumber(item[0].children[1].textContent)
 
       for(let i=1;i<item.length;i++) {
         total = total - getNumber(item[i].children[1].textContent)
-
       }
-    
-      cy.log('total', total)
 
-      if(total != 0) {
-        throw new Error("test fails here")
-      }
+      expect(total).to.equal(0)
     })
 
   })
