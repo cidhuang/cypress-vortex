163bf18465f5b044dc7d9276e56bd29991f30402
Cid Huang <cid@msbt.com.tw>
2023-05-01 16:47:48 +0800

    add: camera layout and player control

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,5 +1,13 @@
 /// <reference types="cypress" />
 
+Cypress.Commands.add('setLayout', (index) => {
+  cy.get('.general_change_layout').click()
+  cy.get('div[data-modal="layout"]').as('layout-modal')
+  cy.get('@layout-modal').find('.layout-list').find('.layout-item').as('layout-list')
+  cy.get('@layout-list').eq(index).click()
+  cy.get('@layout-modal').find('.primary').click()
+})
+
 context('Login', () => {
   beforeEach(() => {
     cy.session('login', () => {
@@ -21,13 +29,54 @@ context('Login', () => {
       ' Message center ',
       ' Archive '
     ]
-  cy.get('.top-button-list').children().find('.label').find('h5').then(item => {
+    cy.get('.top-button-list').children().find('.label').find('h5').then(item => {
       for(let i=0;i<item.length;i++) {
-        cy.log(i, item[i].textContent)
         expect(item[i].textContent).to.equal(topButtonLabel[i])
       }
     })
   })
+  //*/
+  //*
+  it('camera layout', () => {
+    cy.visit('https://portal.vortexcloud.com/#/group')
+
+    function checkLayout(index, gridCount) {
+      cy.setLayout(index)
+      cy.get('.camera-wrapper').should('have.length', gridCount)
+    }
+    const gridCounts = [1, 4, 9, 16, 6, 8, 13, 4, 7, 5, 7, 5, 3]
+    gridCounts.forEach((value, index) => {
+      checkLayout(index, value)
+      for(let i=0;i<value;i++) {
+        if(i === 0) {
+          cy.get('.camera-wrapper').eq(i).find('.view-cell-status').not('.camera-disconnected').should('have.length', 1)
+        }
+        else if(i === 1) {
+          cy.get('.camera-wrapper').eq(i).find('.view-cell-status').find('.camera-disconnected').should('have.length', 1)
+        } else {
+          cy.get('.camera-wrapper').eq(i).not('.view-cell-status').should('have.length', 1)
+        }
+      }
+    });
+  })
+  //*/
+  //*
+  it('camera player control', () => {
+    cy.visit('https://portal.vortexcloud.com/#/group')
+    cy.setLayout(0)
+    cy.setLayout(1)
+    cy.get('.camera-wrapper').eq(0).click()
+
+    cy.get('.event-only-button').click()
+
+    cy.get('.playback-control').find('.go-live-button').click()
+
+    cy.get('.viewcell-wrapper').find('.cell').eq(0)
+      .find('.display-status-bar')
+      .find('.status_hint_live_1st_line_dark')
+      .should('have.length', 1)
+  })
+  //*/
   //*
   it('device count', () => {
     cy.visit('https://portal.vortexcloud.com/#/device/management')
